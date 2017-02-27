a59e324435ad578c25fbeb104b5dc0885b172150
Mojtaba Dashtinejad <mojtaba@gmail.com>
2017-02-27 13:34:56 +0330

    import inside postcss files

--- postcss.config.js
+++ postcss.config.js
@@ -1,11 +1,13 @@
 var cssvariables = require('postcss-css-variables')
 var autoprefixer = require('autoprefixer');
 var nested = require('postcss-nested');
+var partial = require('postcss-import');
 
 module.exports = {
   plugins: [
+    partial(),
+    nested(),
     cssvariables(),
     autoprefixer(),
-    nested(),
   ]
 }
