b2e84bcf0eef67d1e05956308ce759e48f3f680a
Mojtaba Dashtinejad <mojtaba@gmail.com>
2017-02-27 14:16:22 +0330

    postcss color functions

--- postcss.config.js
+++ postcss.config.js
@@ -2,10 +2,14 @@ var cssvariables = require('postcss-css-variables')
 var autoprefixer = require('autoprefixer');
 var nested = require('postcss-nested');
 var partial = require('postcss-import');
+var color = require('postcss-color-function');
+var customProperties = require("postcss-custom-properties")
 
 module.exports = {
   plugins: [
     partial(),
+    customProperties(),
+    color(),    
     nested(),
     cssvariables(),
     autoprefixer(),
